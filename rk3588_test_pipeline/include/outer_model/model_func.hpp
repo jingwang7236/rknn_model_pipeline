43bec4a89e2660c7171dd10a0c976a5e819b2037
chenjun <chenjun97@stu.xmu.edu.cn>
2025-01-07 08:10:35 +0000

    2025-01-07: modify cmakelist

--- rk3588_test_pipeline/include/outer_model/model_func.hpp
+++ rk3588_test_pipeline/include/outer_model/model_func.hpp
@@ -20,17 +20,17 @@ object_detect_result_list inference_person_det_model(rknn_app_context_t *app_ctx
 
 ppocr_text_recog_array_result_t inference_ppocr_det_rec_model(ppocr_system_app_context *rknn_app_ctx, det_model_input input_data, bool enable_logger);
 
-object_detect_result_list inference_det_knife_model(rknn_app_context_t* app_ctx, det_model_input input_data, char* label_txt_path, bool enable_logger);
+object_detect_result_list inference_det_knife_model(rknn_app_context_t* app_ctx, det_model_input input_data, const char* label_txt_path,bool enable_logger);
 
-object_detect_result_list inference_det_gun_model(rknn_app_context_t* app_ctx, det_model_input input_data, char* label_txt_path, bool enable_logger);
+object_detect_result_list inference_det_gun_model(rknn_app_context_t* app_ctx, det_model_input input_data, const char* label_txt_path,bool enable_logger);
 
-object_detect_result_list inference_det_stat_door_model(rknn_app_context_t* app_ctx, det_model_input input_data, char* label_txt_path, bool enable_logger);
+object_detect_result_list inference_det_stat_door_model(rknn_app_context_t* app_ctx, det_model_input input_data, const char* label_txt_path, bool enable_logger);
 
 // resnet rec ren
 resnet_result inference_rec_person_resnet18_model(rknn_app_context_t *app_ctx, det_model_input input_data, bool enable_logger);
 
 // det hand
-object_detect_result_list inference_det_hand_model(rknn_app_context_t *app_ctx, det_model_input input_data, bool enable_logger);
+object_detect_result_list inference_det_hand_model(rknn_app_context_t *app_ctx, det_model_input input_data, bool det_by_square = true, bool enable_logger = true);
 
 
 #endif // _RKNN_DET_CLS_FUNC_H_
